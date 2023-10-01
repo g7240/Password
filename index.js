d9cd12a45ea227537b10899b309b2bc2bfe7cb85
g7240 <107947650+g7240@users.noreply.github.com>
2023-10-01 19:47:36 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,19 +4,16 @@ const app = express()
 const port = process.env.PORT || 9000
 
 
-
 app.use(express.static(__dirname + '/public/'))
 app.use('/favicon.ico', express.static(__dirname + '/public/images/immagine.png'));
 /** Servo pagine statiche */
 app.get('/Autore', (req, res) => {
   res.send("<h1>Autore: Gabriele Bellini</h1><p>Studente di Sicurezza dei Sistemi e delle Reti Informatiche all'UniMI</p>")
 })
-app.get('/articolo-password.pdf', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile("./public/articoloPassword.pdf",{root:__dirname})
 })
-app.get('/', (req, res) => {
-  res.sendFile("./public/home.html",{root:__dirname})
-})
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })
