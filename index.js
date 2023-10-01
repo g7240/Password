e8f458f67c3932c8997d20682b2d3881199bc5ad
g7240 <107947650+g7240@users.noreply.github.com>
2023-10-01 18:43:48 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,30 +4,19 @@ const app = express()
 const port = process.env.PORT || 9000
 
 
-let date_ob = new Date(Date.now());
-let date = date_ob.getDate();
-let month = date_ob.getMonth() + 1;
-let year = date_ob.getFullYear();
-let contatore_download= 0;
-
-
 
 app.use(express.static(__dirname + '/public/'))
 app.use('/favicon.ico', express.static(__dirname + '/public/images/immagine.png'));
 /** Servo pagine statiche */
 app.get('/Autore', (req, res) => {
-  console.log("rispondo alla richiesta Autore.")
   res.send("<h1>Autore: Gabriele Bellini</h1><p>Studente di Sicurezza dei Sistemi e delle Reti Informatiche all'UniMI</p>")
 })
-app.get('/', (req, res) => {
-  contatore_download= contatore_download +1;
-  console.log("Articolo numero "+contatore_download+" dal "+year + "-" + month + "-" + date)
+app.get('/articolo-password.pdf', (req, res) => {
   res.sendFile("./public/articoloPassword.pdf",{root:__dirname})
 })
-app.get('/Accessi', (req, res) => {
-  res.send("<h1>Ci sono state "+contatore_download+" visualizzazzioni dell'articolo a partire dal "+year + "-" + month + "-" + date +"</h1>");
+app.get('/', (req, res) => {
+  res.sendFile("./public/home.html",{root:__dirname})
 })
-
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })
